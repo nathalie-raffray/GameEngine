97978e7c6f107a909ae51215bb5386ce7cd1d73a
Nathalie Raffray <nathalie.raffray9@gmail.com>
2020-04-21 18:05:19 -0400

    Configured project with imgui-sfml and created window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,42 @@
-#include <SFML/Window.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "My window");
+	sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+	window.setFramerateLimit(60);
+	ImGui::SFML::Init(window);
 
-	// run the program as long as the window is open
-	while (window.isOpen())
-	{
-		// check all the window's events that were triggered since the last iteration of the loop
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	sf::Clock deltaClock;
+	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
+
+			if (event.type == sf::Event::Closed) {
 				window.close();
+			}
 		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::End();
+
+		window.clear();
+		window.draw(shape);
+		ImGui::SFML::Render(window);
+		window.display();
 	}
 
-	return 0;
+	ImGui::SFML::Shutdown();
 }
