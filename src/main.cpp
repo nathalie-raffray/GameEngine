183a93c7e415fbbfd1fc5574e490b92e94be4a42
Nathalie Raffray <nathalie.raffray9@gmail.com>
2020-04-21 17:18:48 -0400

    Created a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
-	std::cout << "Hellooooo";
+	sf::Window window(sf::VideoMode(800, 600), "My window");
+
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
+	return 0;
 }
